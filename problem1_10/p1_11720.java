a36e1ab10c98f15a20c9c79bd9d0f8a1e4b153d6
Kim Daul <elre519@naver.com>
2025-10-09 21:35:20 +0900

    10/09

---
+++ problem1_10/p1_11720.java
@@ -0,0 +1,29 @@
+package problem1_10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p1_11720 {
+
+  public static void main(String[] args) throws IOException {
+    /// 모든 입력값 사용하기.
+    // 입력은 2번. 1) 하나는 갯수, 2) 1번 만큼의 숫자열
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int num = Integer.parseInt(br.readLine());
+    int sum = 0;
+    String numbers = br.readLine();
+
+    int[] nums = new int[num];
+
+    for (int i = 0; i < num; i++) {
+      nums[i] = Integer.parseInt(String.valueOf(numbers.charAt(i))); // parseInt는 String을 받음
+    }
+
+    //char[] nums = br.readLine().toCharArray();
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i] /*- '0'*/;
+    }
+    System.out.println(sum);
+  }
+}
---
+++ problem1_10/p2_1546.java
@@ -0,0 +1,31 @@
+package problem1_10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p2_1546 {
+
+  public static void main(String[] args) throws IOException {
+    /// 모든 입력 사용하기 b1
+    // 1) 입력 받을 때 공백이 있음. token 사용가능
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int num = Integer.parseInt(br.readLine());
+    int max = 0;
+    double sum = 0;
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    br.close();
+
+    int[] nums = new int[num];
+    for (int i = 0; i < num; i++) {
+      nums[i] = Integer.parseInt(st.nextToken());
+      sum += nums[i];
+      if (nums[i] > max) {
+        max = nums[i];
+      }
+    }
+    System.out.println((sum/max)*100/num);
+  }
+}
