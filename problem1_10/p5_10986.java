c6942e6780896dc3cb201e33f9ed3a2432d15e11
Kim Daul <elre519@naver.com>
2025-10-11 23:59:42 +0900

    10/11

---
+++ problem1_10/p5_10986.java
@@ -0,0 +1,38 @@
+package problem1_10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p5_10986 {
+
+  /// g3
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int num = Integer.parseInt(st.nextToken());
+    int target = Integer.parseInt(st.nextToken());
+
+    long[] sumArr = new long[num + 1];
+    long[] countArr = new long[target];
+    long result = 0;
+
+    st = new StringTokenizer(br.readLine());
+    for (int i = 1; i <= num; i++) {
+      sumArr[i] = sumArr[i - 1] + Long.parseLong(st.nextToken());
+      long remainder = sumArr[i] % target;
+
+      if (remainder == 0) result++;
+      countArr[(int) remainder]++;
+    }
+
+    for (int i = 0; i < target; i++) {
+      if (countArr[i] > 1) {
+        result += (countArr[i] * (countArr[i] - 1)) / 2;
+      }
+    }
+
+    System.out.println(result);
+  }
+}
---
+++ problem1_10/p6_2018.java
@@ -0,0 +1,32 @@
+package problem1_10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p6_2018 {
+/// s5
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int num = Integer.parseInt(br.readLine());
+    int count = 1;
+    int end = 1;
+    int start = 1;
+    int sum = 1;
+
+    while (end != num) {
+      if (sum == num) {
+        count++;
+        end++;
+        sum= sum + end;
+      } else if (sum > num) {
+        sum = sum - start;
+        start++;
+      } else {
+        end++;
+        sum = sum + end;
+      }
+    }
+    System.out.println(count);
+  }
+}
